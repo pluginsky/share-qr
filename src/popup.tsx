a5e8bb66f89b3113cd21ba243edcd83ff76fde37
Jakub Biesiada <biesiadajakub@icloud.com>
2020-01-22 20:22:47 +0100

    Add style

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,18 +1,42 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import extension from 'extensionizer';
 import QrCode from 'qrcode';
 
+import './popup.css';
+
 const Popup = () => {
   const ref = useRef();
 
+  const [url, setUrl] = useState('');
+  const [error, setError] = useState('');
+
+  const supportedProtocols = ['http', 'https'];
+
   extension.tabs.query({ currentWindow: true, active: true }, (res: any) => {
-    QrCode.toCanvas(ref.current, res[0].url, {
-      width: 260
-    });
+    const currentPageProtocol = res[0].url.split(':')[0];
+
+    if (supportedProtocols.includes(currentPageProtocol)) {
+      setUrl(res[0].url);
+
+      QrCode.toCanvas(ref.current, res[0].url, {
+        width: 260
+      });
+    } else {
+      setError(`Protocol ${currentPageProtocol} is not supported`);
+    }
   });
 
-  return <canvas ref={ref} />;
+  return error ? (
+    <p>{error}</p>
+  ) : (
+    <>
+      <canvas ref={ref} />
+
+      <h4>QR code represents:</h4>
+      <p>{url}</p>
+    </>
+  );
 };
 
 ReactDOM.render(<Popup />, document.getElementById('root'));
