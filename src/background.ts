db02f3b128183de84184bf154589a30ee863b7ab
Jakub Biesiada <biesiadajakub@icloud.com>
2020-06-05 22:47:35 +0200

    fix: open popup in chrome programmatically

--- src/background.ts
+++ src/background.ts
@@ -3,6 +3,11 @@ import extension from 'extensionizer';
 import { Tab } from './shared/enums/Tab';
 import { StoreKey } from './shared/enums/StoreKey';
 
+interface Info {
+  readonly menuItemId: string;
+  readonly selectionText: string;
+}
+
 const CONTEXT_MENU_ID = 'share-as-qr-code';
 
 extension.runtime.onStartup.addListener(() => {
@@ -15,13 +20,13 @@ extension.contextMenus.create({
   contexts: ['selection'],
 });
 
-extension.contextMenus.onClicked.addListener((info: any) => {
+extension.contextMenus.onClicked.addListener((info: Info) => {
   if (info.menuItemId === CONTEXT_MENU_ID) {
     extension.storage.local.set({
       [StoreKey.SelectedText]: info.selectionText,
       [StoreKey.CurrentTab]: Tab.Text,
     });
 
-    extension.browserAction.openPopup();
+    extension.browserAction.openPopup(() => null);
   }
 });
