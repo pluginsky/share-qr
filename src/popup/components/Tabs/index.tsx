1c9e1f4ef8c7bd624616723836f26cbbb1c32984
Jakub Biesiada <biesiadajakub@icloud.com>
2020-07-11 21:25:18 +0200

    chore: prepare update

--- src/popup/components/Tabs/index.tsx
+++ src/popup/components/Tabs/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 
 import { Tab } from '../../../shared/enums/Tab';
 
@@ -10,23 +10,35 @@ interface Props {
   onChange(value: Tab): void;
 }
 
-const Tabs: React.FC<Props> = ({ items, active, onChange }) => (
-  <nav className="tab-navigation">
-    {Object.entries(items).map(([key, value]) => (
-      <div className="tab-navigation__item" key={key}>
-        <input
-          name={name}
-          value={key}
-          type="radio"
-          id={key}
-          checked={active === key}
-          onChange={(e) => onChange(e.target.value as Tab)}
-        />
+const Tabs: React.FC<Props> = ({ items, active, onChange }) => {
+  const [position, setPosition] = useState(0);
 
-        <label htmlFor={key}>{value}</label>
-      </div>
-    ))}
-  </nav>
-);
+  const size = useMemo(() => 100, []);
+
+  return (
+    <nav className="tab-navigation">
+      <div
+        className="tab-navigation__selector"
+        style={{ width: size, left: size * Object.keys(items).indexOf(active) }}
+      />
+
+      {Object.entries(items).map(([key, value]) => (
+        <div className="tab-navigation__item" key={key}>
+          <input
+            name={name}
+            value={key}
+            type="radio"
+            id={key}
+            checked={active === key}
+            onClick={(e) => console.log(e.target)}
+            onChange={(e) => onChange(e.target.value as Tab)}
+          />
+
+          <label htmlFor={key}>{value}</label>
+        </div>
+      ))}
+    </nav>
+  );
+};
 
 export default Tabs;
