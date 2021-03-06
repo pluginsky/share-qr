6777f604f0407c620055a1e8edd0cbeb2bf08922
Jakub Biesiada <biesiadajakub@icloud.com>
2021-03-06 18:55:23 +0100

    fix: disable selection

--- src/popup/components/CodePreview/index.tsx
+++ src/popup/components/CodePreview/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useQrEncode } from 'react-qr-hooks';
 
+import './CodePreview.scss';
+
 interface CodePreviewProps {
   readonly decoded: string;
 }
@@ -10,7 +12,7 @@ const CodePreview = ({ decoded }: CodePreviewProps) => {
     width: 360,
   });
 
-  return <img src={encoded} alt={decoded} />;
+  return <img src={encoded} alt={decoded} className="code-preview" />;
 };
 
 export default CodePreview;
