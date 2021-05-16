9bb774faae34652040090286e667ae86b316dd06
Jakub Biesiada <biesiadajakub@icloud.com>
2021-05-16 15:29:15 +0200

    chore: update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
+module.exports = (_env, argv) => ({
+  mode: argv.mode || 'development',
+  devtool: argv.mode === "production" ? false : "inline-source-map",
   entry: {
     background: './src/background.ts',
     popup: './src/popup/index.tsx',
@@ -32,4 +32,4 @@ module.exports = {
       ],
     }),
   ],
-};
+})
